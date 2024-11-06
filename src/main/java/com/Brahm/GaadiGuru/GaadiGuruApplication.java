8a9fb0b4c1f11e9dde7a12d7e20b95f14a79ace8
Brahmjot1 <brahmjot.nagpal@gmail.com>
2024-11-06 14:55:34 +0530

    initial commit

---
+++ src/main/java/com/Brahm/GaadiGuru/GaadiGuruApplication.java
@@ -0,0 +1,13 @@
+package com.Brahm.GaadiGuru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaadiGuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaadiGuruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Brahm/GaadiGuru/GaadiGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Brahm.GaadiGuru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaadiGuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
